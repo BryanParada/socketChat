a929d3277753c87df56e33897497217e9bcb7967
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-02 09:55:13 -0300

    N270 - Class for controlling users from chat

---
+++ server/classes/user.js
@@ -0,0 +1,57 @@
+
+
+class Users {
+
+    constructor(){
+        this.persons = [];
+    }
+
+    addPerson(id, name){
+
+         let person = {id, name};
+
+         this.persons.push(person);
+
+         return this.persons;
+    }
+
+    getPerson( id ){
+        //OPCION 1
+        // let person = this.persons.filter( pers =>{
+        //     return person.id === id
+        // })[0];
+
+        //OPCION 2
+        let person = this.persons.filter( pers => person.id === id)[0]; //filter regresa un arreglo, por eso siempre necesito la primera posicion
+
+        return person;
+    }
+
+    getPersons(){
+        return this.persons
+    }
+
+    getPersonsByRoom( room ){
+        //...
+    }
+
+    deletePerson(id){
+
+        let deletedPerson = this.getPerson(id);
+
+        //OPCION 1
+        // this.persons = this.persons.filter( pers =>{
+        //     return person.id != id
+        // });
+
+        //OPCION 2
+        this.persons = this.persons.filter( pers => person.id != id);
+
+        return deletedPerson;
+    }
+
+}
+
+
+module.exports = { Users }
+
